da2bbc7fb10bb1fa216ff6bece6b0cc37feb0f9d
Dennis Roberts <dennis@iplantcollaborative.org>
2012-06-07 15:28:24 -0700

    CORE-3245: updated the iplant-service-config release number

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
   :dev-dependencies [[org.iplantc/lein-iplant-rpm "1.1.0-SNAPSHOT"]]
   :iplant-rpm {:summary "scruffian"
                :release 1
-               :dependencies ["iplant-service-config >= 0.1.0-4"]
+               :dependencies ["iplant-service-config >= 0.1.0-5"]
                :config-files ["log4j.properties"]
                :config-path "conf"}
   :aot [scruffian.core]
